724b65d7fc6d0f46b9b788dc2962585876dd9cfc
Kishan Italiya <kishan.italiya@tuvoc.com>
2026-06-03 11:20:36 +0530

    Updated new server URL for backend serving.

--- publish_pod.sh
+++ publish_pod.sh
@@ -19,7 +19,7 @@ fi
 echo "🏷️  Tagging version $VERSION in git..."
 git add .
 #git commit -m "Release version $VERSION"
-git commit -m "Implemented the Gate 1."
+git commit -m "Updated new server URL for backend serving."
 git tag "$VERSION"
 git push origin main
 git push origin "$VERSION"
