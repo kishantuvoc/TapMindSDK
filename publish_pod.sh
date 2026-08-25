3159c8154bea9f4dd5a4dc6d4e6e9530a70209d5
Kishan Italiya <kishan.italiya@tuvoc.com>
2026-08-25 15:55:44 +0530

    Fixed the SPM version mismatch issue

--- publish_pod.sh
+++ publish_pod.sh
@@ -19,7 +19,7 @@ echo "🚀 Starting publication process for $PODSPEC_NAME version $VERSION..."
 echo "🏷️  Tagging version $VERSION in git..."
 git add .
 #git commit -m "Release version $VERSION"
-git commit -m "Fixed the ad refresh issue"
+git commit -m "Fixed the SPM version mismatch issue"
 git tag "$VERSION"
 git push origin main
 git push origin "$VERSION"
